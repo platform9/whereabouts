63996c6723931da972d3a9ed9f1b6318229fd27d
Arjun Baindur <xagent@gmail.com>
2022-01-19 19:30:54 -0800

    ip-reconciler: Use ContainerID instead of PodRef
    refactored on top of control loop changes by Doug

--- pkg/reconciler/iploop.go
+++ pkg/reconciler/iploop.go
@@ -175,10 +175,11 @@ func composePodRef(pod v1.Pod) string {
 }
 
 func (rl ReconcileLooper) ReconcileIPPools(ctx context.Context) ([]net.IP, error) {
-	matchByPodRef := func(reservations []types.IPReservation, podRef string) int {
+	matchByContainerID := func(reservations []types.IPReservation, containerID string) int {
 		foundidx := -1
+		fmt.Printf("!bang ------------- %+v", reservations)
 		for idx, v := range reservations {
-			if v.PodRef == podRef {
+			if v.ContainerID == containerID {
 				return idx
 			}
 		}
@@ -190,11 +191,10 @@ func (rl ReconcileLooper) ReconcileIPPools(ctx context.Context) ([]net.IP, error
 	for _, orphanedIP := range rl.orphanedIPs {
 		// !bang: Reportedly: This the order returned from the below line is not fixed.
 		currentIPReservations := orphanedIP.Pool.Allocations()
-		podRefsToDeallocate := findOutPodRefsToDeallocateIPsFrom(orphanedIP)
+		containerIDsToDeallocate := findOutContainerIDsToDeallocateIPsFrom(orphanedIP)
 		var deallocatedIP net.IP
-		for _, podRef := range podRefsToDeallocate {
-			// !bang: This means it may cause the in-use pod ip to be wrongly claimed
-			currentIPReservations, deallocatedIP, err = allocate.IterateForDeallocation(currentIPReservations, podRef, matchByPodRef)
+		for _, containerID := range containerIDsToDeallocate {
+			currentIPReservations, deallocatedIP, err = allocate.IterateForDeallocation(currentIPReservations, containerID, matchByContainerID)
 			if err != nil {
 				return nil, err
 			}
@@ -258,10 +258,10 @@ func (rl ReconcileLooper) ReconcileOverlappingIPAddresses(ctx context.Context) e
 	return nil
 }
 
-func findOutPodRefsToDeallocateIPsFrom(orphanedIP OrphanedIPReservations) []string {
-	var podRefsToDeallocate []string
+func findOutContainerIDsToDeallocateIPsFrom(orphanedIP OrphanedIPReservations) []string {
+	var containerIDsToDeallocate []string
 	for _, orphanedAllocation := range orphanedIP.Allocations {
-		podRefsToDeallocate = append(podRefsToDeallocate, orphanedAllocation.PodRef)
+		containerIDsToDeallocate = append(containerIDsToDeallocate, orphanedAllocation.ContainerID)
 	}
-	return podRefsToDeallocate
+	return containerIDsToDeallocate
 }
