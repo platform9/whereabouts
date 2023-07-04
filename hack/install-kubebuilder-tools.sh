9c506cd03a88af835350283fd2fc2e87a533e801
Trilok Khairnar <214651+cruizen@users.noreply.github.com>
2023-07-04 20:48:21 +0530

    Update install-kubebuilder-tools.sh

--- hack/install-kubebuilder-tools.sh
+++ hack/install-kubebuilder-tools.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
+BASEDIR=$(pwd)
+mkdir -p ${BASEDIR}/bin
 
 # install controller-gen
-go get sigs.k8s.io/controller-tools/cmd/controller-gen@v0.4.1
+GOBIN=${BASEDIR}/bin go install sigs.k8s.io/controller-tools/cmd/controller-gen@v0.4.1
 
 # install kubebuilder tools to bin/
 mkdir -p bin
