38c600f006d3366794f8019ff017a69e6b904ace
Trilok Khairnar <trilok@platform9.com>
2023-07-05 12:39:49 +0530

    Fix CI : Use go.17 from Github actions build, test

--- hack/install-kubebuilder-tools.sh
+++ hack/install-kubebuilder-tools.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-# install controller-gen
-go get sigs.k8s.io/controller-tools/cmd/controller-gen@v0.4.1
+++BASEDIR=$(pwd)
++ # install controller-gen
++-go get sigs.k8s.io/controller-tools/cmd/controller-gen@v0.4.1
+++GOBIN=${BASEDIR}/bin go install sigs.k8s.io/controller-tools/cmd/controller-gen@v0.4.1
 
 # install kubebuilder tools to bin/
 mkdir -p bin
